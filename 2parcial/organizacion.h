65192759736787e8c896ad545d6bdb1b8e02063e
andresoria2295 <a.soria310895@gmail.com>
2018-11-05 11:06:18 -0300

    Segundo Parcial en repositorio propio

---
+++ 2parcial/organizacion.h
@@ -0,0 +1,31 @@
+#ifndef ORGANIZACION_H
+#define ORGANIZACION_H
+#include <string>
+#include <iostream>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+class Organizacion
+{
+    public:
+        Organizacion();
+        Organizacion(string,int);
+
+        void agregar();
+        void listar();
+        void eliminar(string);
+        void inicio();
+        void setNombre(string);
+        string getNombre();
+        void setid(int);
+        int getid();
+
+    protected:
+
+    private:
+        string nombre;
+        int id;
+};
+#endif // ORGANIZACION_H
